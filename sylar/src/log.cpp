e543ab5d0c04f4387c131f834e62720d31403e44
1294479328@qq.com <1294479328@qq.com>
2023-03-06 14:14:35 +0800

    修改文件结构

--- sylar/src/log.cpp
+++ sylar/src/log.cpp
@@ -1,4 +1,4 @@
-#include "log.h"
+#include "../log.h"
 #include <map>
 #include <functional>
 #include <memory>
@@ -127,7 +127,7 @@ private:
 */
 
 LogEvent::LogEvent(const char* file, int32_t line, uint32_t elapse, uint32_t thread_id, uint32_t fiber_id, uint64_t time) : 
- m_file(file), m_line(m_line), m_elapse(elapse), m_threadId(thread_id), m_fiberId(fiber_id), m_time(time){}
+ m_file(file), m_line(line), m_elapse(elapse), m_threadId(thread_id), m_fiberId(fiber_id), m_time(time){}
 
 Logger::Logger(const std::string& name) : m_name(name), m_level(LogLevel::DEBUG){
     m_formatter.reset(new LogFormatter("%d  [%p] %f %l %m %n"));
@@ -273,7 +273,7 @@ void LogFormatter::init(){
             i = n;
         }else if(fmt_status == 1){
             std::cout << "pattern parse error: " << m_pattern << " - " << m_pattern.substr(i) << std::endl;
-            vec.push_back(std::tuple("<<pattern_error>>", fmt, 0));
+            vec.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
         }else if(fmt_status == 2){
             if(!nstr.empty()){
                 vec.push_back(std::make_tuple(nstr, "", 0)); //
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "log.h"
+#include "../sylar/log.h"
 
 int main(int argc, char** argv){
     sylar::Logger::ptr logger(new sylar::Logger());
