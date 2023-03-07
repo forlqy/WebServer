0cab869c4528e75b59efe29117bb2615b4315339
1294479328@qq.com <1294479328@qq.com>
2023-03-07 19:02:44 +0800

    完成日志和配置模块

--- sylar/log.h
+++ sylar/log.h
@@ -1,7 +1,6 @@
 #ifndef __SYLAR_LOG_H__
 #define __SYLAR_LOG_H__
 
-#include <iostream>
 #include <string>
 #include <stdint.h>
 #include <memory>
@@ -9,39 +8,45 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <stdarg.h>
+#include <map>
+#include "util.h"
+#include "singleton.h"
 
-namespace sylar{
+#define SYLAR_LOG_LEVEL(logger, level) \
+    if(logger->getLevel() <= level) \
+        sylar::LogEventWrap(sylar::LogEvent::ptr(new sylar::LogEvent(logger, level, \
+                        __FILE__, __LINE__, 0, sylar::GetThreadId(),\
+                sylar::GetFiberId(), time(0)))).getSS()
+
+#define SYLAR_LOG_DEBUG(logger) SYLAR_LOG_LEVEL(logger, sylar::LogLevel::DEBUG)
+#define SYLAR_LOG_INFO(logger) SYLAR_LOG_LEVEL(logger, sylar::LogLevel::INFO)
+#define SYLAR_LOG_WARN(logger) SYLAR_LOG_LEVEL(logger, sylar::LogLevel::WARN)
+#define SYLAR_LOG_ERROR(logger) SYLAR_LOG_LEVEL(logger, sylar::LogLevel::ERROR)
+#define SYLAR_LOG_FATAL(logger) SYLAR_LOG_LEVEL(logger, sylar::LogLevel::FATAL)
+
+#define SYLAR_LOG_FMT_LEVEL(logger, level, fmt, ...) \
+    if(logger->getLevel() <= level) \
+        sylar::LogEventWrap(sylar::LogEvent::ptr(new sylar::LogEvent(logger, level, \
+                        __FILE__, __LINE__, 0, sylar::GetThreadId(),\
+                sylar::GetFiberId(), time(0)))).getEvent()->format(fmt, __VA_ARGS__)
+
+#define SYLAR_LOG_FMT_DEBUG(logger, fmt, ...) SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::DEBUG, fmt, __VA_ARGS__)
+#define SYLAR_LOG_FMT_INFO(logger, fmt, ...)  SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::INFO, fmt, __VA_ARGS__)
+#define SYLAR_LOG_FMT_WARN(logger, fmt, ...)  SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::WARN, fmt, __VA_ARGS__)
+#define SYLAR_LOG_FMT_ERROR(logger, fmt, ...) SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::ERROR, fmt, __VA_ARGS__)
+#define SYLAR_LOG_FMT_FATAL(logger, fmt, ...) SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::FATAL, fmt, __VA_ARGS__)
+
+#define SYLAR_LOG_ROOT() sylar::LoggerMgr::GetInstance()->getRoot()
+
+namespace sylar {
+
 class Logger;
-//日志事件    
-class LogEvent{
-public:
-    typedef std::shared_ptr<LogEvent> ptr;
-    LogEvent(const char* file, int32_t m_line, uint32_t elapse, uint32_t thread_id, uint32_t fiber_id, uint64_t time);
-
-    const char* getFile() const{return m_file;}
-    int32_t getLine() const{return m_line;}
-    uint32_t getElapse() const{return m_elapse;}
-    uint32_t getThreadId() const{return m_threadId;}
-    uint32_t getFiberId() const{return m_fiberId;}
-    uint64_t getTime() const{return m_time;}
-    std::string getContent() const{return m_ss.str();}
-
-    std::stringstream& getSS() {return m_ss;}
-private:
-    const char* m_file = nullptr; //文件名(成员变量前加m表示成员变量，用于和方法参数名称相同时冲突)
-    int32_t m_line = 0; //行号
-    uint32_t m_elapse = 0; //程序启动开始到现在的毫秒数 (内存对齐)
-    uint32_t m_threadId = 0; //线程ID
-    uint32_t m_fiberId = 0; //协程ID
-    uint64_t m_time = 0; //时间戳
-    std::stringstream m_ss;
-    std::string m_content;
-};
 
 //日志级别
-class LogLevel{
+class LogLevel {
 public:
-    enum Level{
+    enum Level {
         UNKNOW = 0,
         DEBUG = 1,
         INFO = 2,
@@ -53,48 +58,98 @@ public:
     static const char* ToString(LogLevel::Level level);
 };
 
+//日志事件
+class LogEvent {
+public:
+    typedef std::shared_ptr<LogEvent> ptr;
+    LogEvent(std::shared_ptr<Logger> logger, LogLevel::Level level
+            ,const char* file, int32_t m_line, uint32_t elapse
+            , uint32_t thread_id, uint32_t fiber_id, uint64_t time);
+
+    const char* getFile() const { return m_file;}
+    int32_t getLine() const { return m_line;}
+    uint32_t getElapse() const { return m_elapse;}
+    uint32_t getThreadId() const { return m_threadId;}
+    uint32_t getFiberId() const { return m_fiberId;}
+    uint64_t getTime() const { return m_time;}
+    std::string getContent() const { return m_ss.str();}
+    std::shared_ptr<Logger> getLogger() const { return m_logger;}
+    LogLevel::Level getLevel() const { return m_level;}
+
+    std::stringstream& getSS() { return m_ss;}
+    void format(const char* fmt, ...);
+    void format(const char* fmt, va_list al);
+private:
+    const char* m_file = nullptr;  //文件名
+    int32_t m_line = 0;            //行号
+    uint32_t m_elapse = 0;         //程序启动开始到现在的毫秒数
+    uint32_t m_threadId = 0;       //线程id
+    uint32_t m_fiberId = 0;        //协程id
+    uint64_t m_time = 0;           //时间戳
+    std::stringstream m_ss;
+
+    std::shared_ptr<Logger> m_logger;
+    LogLevel::Level m_level;
+};
+
+class LogEventWrap {
+public:
+    LogEventWrap(LogEvent::ptr e);
+    ~LogEventWrap();
+    LogEvent::ptr getEvent() const { return m_event;}
+    std::stringstream& getSS();
+private:
+    LogEvent::ptr m_event;
+};
+
 //日志格式器
-class LogFormatter{
+class LogFormatter {
 public:
     typedef std::shared_ptr<LogFormatter> ptr;
     LogFormatter(const std::string& pattern);
+
+    //%t    %thread_id %m%n
     std::string format(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event);
-public: //实例
-    class FormatItem{
+public:
+    class FormatItem {
     public:
-        using ptr = std::shared_ptr<FormatItem>;
-        FormatItem(const std::string& fmt = "") {}
+        typedef std::shared_ptr<FormatItem> ptr;
+        virtual ~FormatItem() {}
         virtual void format(std::ostream& os, std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
-        virtual ~FormatItem(){}
     };
+
     void init();
 private:
     std::string m_pattern;
     std::vector<FormatItem::ptr> m_items;
+
 };
 
 //日志输出地
-class LogAppender{
+class LogAppender {
 public:
     typedef std::shared_ptr<LogAppender> ptr;
-    virtual ~LogAppender(){}
+    virtual ~LogAppender() {}
 
     virtual void log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
 
-    void setFormatter(LogFormatter::ptr val){m_formatter = val;}
-    LogFormatter::ptr getFormatter() const{return m_formatter;}
-protected: //虚基类，要设置为protected以便于派生类修改m_level
-    LogLevel::Level m_level;
+    void setFormatter(LogFormatter::ptr val) { m_formatter = val;}
+    LogFormatter::ptr getFormatter() const { return m_formatter;}
+
+    LogLevel::Level getLevel() const { return m_level;}
+    void setLevel(LogLevel::Level val) { m_level = val;}
+protected:
+    LogLevel::Level m_level = LogLevel::DEBUG;
     LogFormatter::ptr m_formatter;
 };
 
 //日志器
-class Logger : public std::enable_shared_from_this<Logger>{ //enable
+class Logger : public std::enable_shared_from_this<Logger> {
 public:
     typedef std::shared_ptr<Logger> ptr;
 
     Logger(const std::string& name = "root");
-    void log(LogLevel::Level level, LogEvent::ptr event); 
+    void log(LogLevel::Level level, LogEvent::ptr event);
 
     void debug(LogEvent::ptr event);
     void info(LogEvent::ptr event);
@@ -104,37 +159,52 @@ public:
 
     void addAppender(LogAppender::ptr appender);
     void delAppender(LogAppender::ptr appender);
-    LogLevel::Level getLevel() const{return m_level;}
-    void setLevel(LogLevel::Level val){m_level = val;}
+    LogLevel::Level getLevel() const { return m_level;}
+    void setLevel(LogLevel::Level val) { m_level = val;}
 
-    const std::string& getName() const {return m_name;}
+    const std::string& getName() const { return m_name;}
 private:
-    std::string m_name; //日志名称
-    LogLevel::Level m_level; //日志级别
-    std::list<LogAppender::ptr> m_appenders; //Appender集合
-    LogFormatter::ptr m_formatter; //输出格式
+    std::string m_name;                     //日志名称
+    LogLevel::Level m_level;                //日志级别
+    std::list<LogAppender::ptr> m_appenders;//Appender集合
+    LogFormatter::ptr m_formatter;
 };
 
 //输出到控制台的Appender
-class StdoutLogAppender : public LogAppender{
+class StdoutLogAppender : public LogAppender {
 public:
-    using ptr = std::shared_ptr<StdoutLogAppender>; //仅在类内部生效的类型别名 此处用using 代替 typedef
-    void log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) override; //bug
+    typedef std::shared_ptr<StdoutLogAppender> ptr;
+    void log(Logger::ptr logger, LogLevel::Level level, LogEvent::ptr event) override;
 };
 
-//输出到文件的Appender
-class FileLogAppender : public LogAppender{  
+//定义输出到文件的Appender
+class FileLogAppender : public LogAppender {
 public:
-    using ptr = std::shared_ptr<FileLogAppender>; 
+    typedef std::shared_ptr<FileLogAppender> ptr;
     FileLogAppender(const std::string& filename);
-    void log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) override;
-    bool reopen(); //重新打开文件，打开成功返回true，否则返回false
+    void log(Logger::ptr logger, LogLevel::Level level, LogEvent::ptr event) override;
+
+    //重新打开文件，文件打开成功返回true
+    bool reopen();
 private:
     std::string m_filename;
     std::ofstream m_filestream;
 };
 
-}
+class LoggerManager {
+public:
+    LoggerManager();
+    Logger::ptr getLogger(const std::string& name);
 
+    void init();
+    Logger::ptr getRoot() const { return m_root;}
+private:
+    std::map<std::string, Logger::ptr> m_loggers;
+    Logger::ptr m_root;
+};
+
+typedef sylar::Singleton<LoggerManager> LoggerMgr;
+
+}
 
 #endif
---
+++ sylar/singleton.h
@@ -0,0 +1,43 @@
+/*这是在写单例模式的模板类
+之所以不在日志管理器里面实现单例模式，以为分布式多线程等可能是每个线程一个单例
+使用模板就可以解决这类问题
+*/
+
+#ifndef __SYLAR_SINGLETON_H__
+#define __SYLAR_SINGLETON_H__
+
+namespace sylar {
+
+template<class T, class X = void, int N = 0>
+class Singleton {
+public:
+    static T* GetInstance() {
+        static T v;
+        return &v;
+    }
+};
+
+template<class T, class X = void, int N = 0>
+class SingletonPtr {
+public:
+    static std::shared_ptr<T> GetInstance() {
+        static std::shared_ptr<T> v(new T);
+        return v;
+    }
+};
+
+}
+
+#endif
+
+
+
+
+
+
+
+
+/*
+ 这里使用模板类实现单例，日志管理器本身不是单例，模板类是一个提供的单例构造器，只要静态方法就行了
+ 构造的单例类的构造函数不能私有，因为模板类静态函数需要调用
+  */
